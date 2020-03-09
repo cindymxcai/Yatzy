ac6edb09f169bd6b37fd8e97f2bc6b20c04a8cae
Cindy Cai <Cindy.cai@myob.com>
2020-03-09 13:44:41 +1300

    chance, yatzy and numbers

--- DiceTests/DiceTest.cs
+++ DiceTests/DiceTest.cs
@@ -2,11 +2,19 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace DiceTests
 {
     public class DiceTests
     {
+        private readonly ITestOutputHelper _testOutputHelper;
+
+        public DiceTests(ITestOutputHelper testOutputHelper)
+        {
+            _testOutputHelper = testOutputHelper;
+        }
+
         [Fact]
         public void DiceRollShouldChangeValue()
         {
@@ -26,19 +34,98 @@ namespace DiceTests
             int heldDice = dice.RollDice();
             Assert.Equal((object)rollDice, (object)heldDice);
         }
+        
 
         [Fact]
-        public void DiceSum()
+        public void DiceSumAndChanceTest()
         {
             var calc = new ScoreCalculator();
-            var dice = new Dice();
-            int rollFirstDice = dice.RollDice();
-            int rollSecondDice = dice.RollDice();
+            int diceOne = 3;
+            int diceTwo = 2;
+            int diceThree = 4;
             List<int> dices = new List<int>();
-            dices.Add(rollFirstDice);
-            Assert.Equal(calc.getSumOfDice(rollFirstDice, dices), rollFirstDice );
-            
+            dices.Add(diceOne);
+            dices.Add(diceTwo);
+            dices.Add(diceThree);
+           Assert.Equal(9, calc.getSumOfDice(dices));
         }
+
+        [Fact]
+        public void YatzyTest()  //breaks if last dice is 3 as should but doesnt if 2? anything greater is out of range?
+        {
+            var calc = new ScoreCalculator();
+            int diceOne = 1;
+            int diceTwo = 1;
+            int diceThree = 1;
+            int diceFour = 1;
+            int diceFive = 2;
+            List<int> dices = new List<int>();
+            dices.Add(diceOne);
+            dices.Add(diceTwo);
+            dices.Add(diceThree);
+            dices.Add(diceFour);
+            dices.Add(diceFive);
+            Assert.Equal(50, calc.Yatzy(dices));
+        }
+        
+        [Fact]
+        public void NonYatzyTest()
+        {
+            var calc = new ScoreCalculator();
+            int diceOne = 1;
+            int diceTwo = 1;
+            int diceThree = 3;
+            int diceFour = 3;
+            int diceFive = 1;
+            List<int> dices = new List<int>();
+            dices.Add(diceOne);
+            dices.Add(diceTwo);
+            dices.Add(diceThree);
+            dices.Add(diceFour);
+            dices.Add(diceFive);
+            Assert.Equal(0, calc.Yatzy(dices));
+        }
+
+
+        [Fact]
+        public void OnesTest()
+        {
+            var calc = new ScoreCalculator();
+            int diceOne = 2;
+            int diceTwo = 4;
+            int diceThree = 3;
+            int diceFour = 3;
+            int diceFive = 5;
+            List<int> dices = new List<int>();
+            dices.Add(diceOne);
+            dices.Add(diceTwo);
+            dices.Add(diceThree);
+            dices.Add(diceFour);
+            dices.Add(diceFive);
+            
+            Assert.Equal(0, calc.Ones(dices));
+        }
+        [Fact]
+        public void FoursTest()
+        {
+            var calc = new ScoreCalculator();
+            int diceOne = 1;
+            int diceTwo = 4;
+            int diceThree = 3;
+            int diceFour = 3;
+            int diceFive = 4;
+            List<int> dices = new List<int>();
+            dices.Add(diceOne);
+            dices.Add(diceTwo);
+            dices.Add(diceThree);
+            dices.Add(diceFour);
+            dices.Add(diceFive);
+            
+            Assert.Equal(8, calc.Fours(dices));
+        }
+        
+        
+        
         
     }
 }
--- YatzyKata/ScoreCalculator.cs
+++ YatzyKata/ScoreCalculator.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 
 namespace DiceTests
 {
@@ -6,12 +7,111 @@ namespace DiceTests
     {
 
         public int DiceSum = 0;
-        public int getSumOfDice(int diceValue, List<int> dices)
+  
+
+
+        public int getSumOfDice(List<int> dices)
         {
-            foreach (var die in dices)
+            foreach (var dice in  dices)
             {
-                DiceSum += diceValue;
+                DiceSum += dice;
             }
+
+            return DiceSum;
+        }
+
+        public int Yatzy(List<int> dices)
+        {
+            int yatzyStreak = 0;
+
+            foreach (var dice in dices)
+            {
+                if (dices[dice] == dices[dice+1])
+                {
+                     yatzyStreak++;
+                }
+
+                if (yatzyStreak > 4)
+                {
+                    DiceSum = 50;
+                }
+            }
+
+            return DiceSum;
+        }
+
+        public int Ones(List<int> dices) //find better way to do this!!
+        {
+            foreach (var dice in dices)
+            {
+                if (dice == 1)
+                {
+                    DiceSum+= dice;
+                }
+            }
+            return DiceSum;
+        }
+
+        
+        public int Twos(List<int> dices)
+        {
+            foreach (var dice in dices)
+            {
+                if (dice == 2)
+                {
+                    DiceSum+= dice;
+                }
+            }
+            return DiceSum;
+        }
+        
+        public int Threes(List<int> dices)
+        {
+            foreach (var dice in dices)
+            {
+                if (dice == 3)
+                {
+                    DiceSum+= dice;
+                }
+            }
+            return DiceSum;
+        }
+        
+        
+        public int Fours(List<int> dices)
+        {
+            foreach (var dice in dices)
+            {
+                if (dice == 4)
+                {
+                    DiceSum+= dice;
+                }
+            }
+            return DiceSum;
+        }
+        
+        public int Fives(List<int> dices)
+        {
+            foreach (var dice in dices)
+            {
+                if (dice == 5)
+                {
+                    DiceSum+= dice;
+                }
+            }
+            return DiceSum;
+        }
+
+        public int Sixes(List<int> dices)
+        {
+            foreach (var dice in dices)
+            {
+                if (dice == 6)
+                {
+                    DiceSum += dice;
+                }
+            }
+
             return DiceSum;
         }
         
